93882d5df91006f8f4f2f232a17dd9fd70e2183d
Peter Lindblom <peter.lindblom@teliacompany.com>
2018-08-21 10:34:38 +0200

    updated example

--- examples/default/example.tf
+++ examples/default/example.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "us-west-2"
 }
 
-module "vpc" {
+module "vpc1" {
   source               = "../../"
-  name_prefix          = "example"
+  name_prefix          = "example1"
   cidr_block           = "10.8.0.0/16"
   private_subnet_count = "2"
   create_public_subnets = "false"
@@ -17,10 +17,32 @@ module "vpc" {
   }
 }
 
-output "vpc_id" {
-  value = "${module.vpc.vpc_id}"
+module "vpc2" {
+  source               = "../../"
+  name_prefix          = "example2"
+  cidr_block           = "10.9.0.0/16"
+  private_subnet_count = "3"
+  create_public_subnets = "true"
+
+  enable_dns_hostnames = "true"
+
+  tags {
+    environment = "prod"
+    terraform   = "True"
+  }
+}
+output "vpc_id1" {
+  value = "${module.vpc1.vpc_id}"
 }
 
-output "subnet_ids" {
-  value = "${module.vpc.public_subnet_ids}"
+output "vpc_id2" {
+  value = "${module.vpc2.vpc_id}"
+}
+
+output "public_subnet_ids" {
+  value = "${module.vpc1.public_subnet_ids}"
+}
+
+output "private_subnet_ids" {
+  value = "${module.vpc2.private_subnet_ids}"
 }
