cd2dda422fbeed9c73b6e5643339955fe7c121a2
itsdalmo <kristian@doingit.no>
2019-07-05 18:21:22 +0200

    Fix formatting of terraform block

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,5 +1,6 @@
 terraform {
   required_version = ">= 0.12"
+
   backend "s3" {
     key            = "terraform-modules/development/terraform-aws-vpc/default.tfstate"
     bucket         = "<test-account-id>-terraform-state"
