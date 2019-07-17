45514e57ffb979969e3845713d3ec100a9fba46a
itsdalmo <kristian@doingit.no>
2019-07-17 12:21:41 +0200

    Fix syntax and examples

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -8,17 +8,24 @@ provider "aws" {
 }
 
 module "vpc" {
-  source                 = "../../"
-  name_prefix            = var.name_prefix
-  cidr_block             = "10.100.0.0/16"
-  create_nat_gateways    = false
-  enable_dns_hostnames   = true
-  create_private_subnets = true
+  source      = "../../"
+  name_prefix = var.name_prefix
+  cidr_block  = "10.100.0.0/16"
 
+  availability_zones = [
+    "eu-west-1a",
+    "eu-west-1b",
+    "eu-west-1c",
+  ]
+
+  create_private_subnets = true
   private_subnet_cidrs = [
     "10.100.48.0/20",
     "10.100.64.0/20",
   ]
+
+  create_nat_gateways  = true
+  enable_dns_hostnames = true
 
   tags = {
     terraform   = "True"
