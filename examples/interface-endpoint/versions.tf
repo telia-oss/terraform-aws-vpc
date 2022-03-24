9ab3e737b630a8fe072d460ca7d6757668da9a12
Dalibor Lis <dalibor.lis@telia.no>
2022-03-24 08:41:05 +0100

    Increase minimal version of AWS provider for tests

--- examples/interface-endpoint/versions.tf
+++ examples/interface-endpoint/versions.tf
@@ -1,9 +1,9 @@
 terraform {
   required_providers {
     aws = {
-      version = "~> 3.0"
+      version = "~> 4.0"
       source  = "hashicorp/aws"
     }
   }
-  required_version = ">= 0.14"
+  required_version = ">= 1.1.7"
 }
